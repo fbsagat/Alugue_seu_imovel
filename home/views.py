3a82fb90ca19ca47ece18dfe9e6e8b3c53364275
Fábio <www.fabioaugusto@hotmail.com>
2023-03-10 17:47:43 -0300

    Melhorias e correções

--- home/views.py
+++ home/views.py
@@ -26,8 +26,6 @@ from home.forms import FormCriarConta, FormHomePage, FormMensagem, FormEventos, 
 
 from home.models import Locatario, Contrato, Pagamento, Gasto, Anotacoe, ImovGrupo, Usuario, Imovei, Parcela, Tarefa
 
-locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
-
 
 # -=-=-=-=-=-=-=-= BOTÃO DASHBOARD -=-=-=-=-=-=-=-=
 
