54acaaa01f5be9cdf449059fd2fe4606261a48e8
Fábio <www.fabioaugusto@hotmail.com>
2023-03-10 19:41:33 -0300

    Melhorias e correções

--- Alugue_seu_imovel/settings.py
+++ Alugue_seu_imovel/settings.py
@@ -98,7 +98,7 @@ elif USAR_DB == 3:
     # PostGreSQL + railway.app ( with dj-database-url)
     DATABASE_URL = os.getenv('DATABASE_URL')
     if DATABASE_URL:
-        databases = {'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=1800)}
+        DATABASES = {'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=1800)}
 
     # DATABASES = {'default': {'ENGINE': 'django.db.backends.postgresql',
     #                          'NAME': 'railway',
