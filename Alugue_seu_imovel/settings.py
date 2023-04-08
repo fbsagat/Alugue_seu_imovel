e984cc15e749267dfe6e8592a4be027802270d38
Fábio <www.fabioaugusto@hotmail.com>
2023-04-08 01:18:57 -0300

    Melhorias e correções de bugs da função gerador de contrato pdf.

--- Alugue_seu_imovel/settings.py
+++ Alugue_seu_imovel/settings.py
@@ -125,9 +125,9 @@ elif USAR_DB == 2:
         DATABASES = {'default': {'ENGINE': 'django.db.backends.postgresql',
                                  'NAME': 'railway',
                                  'USER': 'postgres',
-                                 'PASSWORD': '4ltXtJKKMKSzmuVnAzYB',
-                                 'HOST': 'containers-us-west-12.railway.app',
-                                 'PORT': '5753',
+                                 'PASSWORD': 'AhGcOE5XJilzIqfHv6kY',
+                                 'HOST': 'containers-us-west-195.railway.app',
+                                 'PORT': '6191',
                                  }
                      }
 elif USAR_DB == 3:
