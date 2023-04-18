c04f63c811bc72ad813276689b742776da949aaf
Fabio <www.fabioaugusto@hotmail.com>
2023-04-18 13:19:44 -0300

    Preparando para o servidor de testes(railway).

--- Alugue_seu_imovel/settings.py
+++ Alugue_seu_imovel/settings.py
@@ -179,7 +179,7 @@ if USAR_DB == 3 or 2:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static').replace('\\', '/'), ]
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/').replace('\\', '/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media').replace('\\', '/')
 MEDIA_URL = '/media/'
 
 # Default primary key field type
